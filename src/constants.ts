9aff89c51de46c362634edbd9756ae590a58d14d
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-01-01 00:12:35 +0900

    4기생 오디션 서류 합격자 반영

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
 type Streamer = { readonly id: string; readonly nick: string };
 
 export const STREAMER_LIST: readonly Streamer[] = [
-  { id: 'dlsn9911', nick: '제갈금자' },
-  { id: '9mogu9', nick: '모구구' },
-  { id: 'haroha', nick: '하로하' },
-  { id: 'kgoyangyeeee', nick: '누눙지' },
-  { id: 'wjdfogur98', nick: '대월향' },
+  { id: 'kyaru96', nick: '캬루' },
+  { id: 'usharko0o0', nick: '유샥크' },
+  { id: 'b4bycthu1hu', nick: '데로' },
+  { id: 'onyu98', nick: '에냐' },
+  { id: 'idenq9', nick: '아이덴큐' },
+  { id: 'yaguja00', nick: '야구자' },
+  { id: 'denebeu', nick: '데네브' },
 ];
