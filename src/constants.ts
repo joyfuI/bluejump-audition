f18b6980bbbfe13f9eeed30c73e485621a0bc01a
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-01-01 13:28:03 +0900

    치지직 스트리머 추가

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,10 @@
 type Streamer = { readonly id: string; readonly nick: string };
 
 export const STREAMER_LIST: readonly Streamer[] = [
+  { id: 'jihan110', nick: '지한이또' },
   { id: 'kyaru96', nick: '캬루' },
   { id: 'usharko0o0', nick: '유샥크' },
+  { id: 'yonakayo05', nick: 'YONAKA요나카' },
   { id: 'b4bycthu1hu', nick: '데로' },
   { id: 'onyu98', nick: '에냐' },
   { id: 'idenq9', nick: '아이덴큐' },
