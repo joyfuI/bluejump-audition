a7393b38cda259eec6748a2dd21972b4c6edff9c
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-01-04 20:49:22 +0900

    1차 오디션 합격자 반영

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,9 @@
 type Streamer = { readonly id: string; readonly nick: string };
 
 export const STREAMER_LIST: readonly Streamer[] = [
-  { id: 'jihan110', nick: '지한이또' },
-  { id: 'kyaru96', nick: '캬루' },
   { id: 'usharko0o0', nick: '유샥크' },
   { id: 'yonakayo05', nick: 'YONAKA요나카' },
   { id: 'b4bycthu1hu', nick: '데로' },
-  { id: 'onyu98', nick: '에냐' },
-  { id: 'idenq9', nick: '아이덴큐' },
   { id: 'yaguja00', nick: '야구자' },
   { id: 'denebeu', nick: '데네브' },
 ];
