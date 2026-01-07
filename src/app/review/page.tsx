df88af025dcabb48248e9371a28019d5bef35fa5
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-01-07 14:49:31 +0900

    다시보기 모아보기도 클라이언트 패치로 변경

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,38 +1,24 @@
+'use client';
 import { Alert, Flex } from 'antd';
 
-import type { GetVodsReviewResponse } from '@/api/getVodsReview';
-import getVodsReview, { REVALIDATE } from '@/api/getVodsReview';
 import ClientLayout from '@/components/ClientLayout';
-import { STREAMER_LIST } from '@/constants';
-import dayjs from '@/lib/dayjs';
 
 import FilterButton from './FilterButton';
+import useReviewQuery from './hooks/useReviewQuery';
 import VodCard from './VodCard';
 
-const Page = async () => {
-  const vodsReviewListPromise = STREAMER_LIST.map((item) =>
-    getVodsReview(item.id),
-  );
-  const vodsReviewList = (await Promise.all(vodsReviewListPromise))
-    .reduce<GetVodsReviewResponse['data']>(
-      (prev, curr) => prev.concat(curr.data),
-      [],
-    )
-    .toSorted((a, b) => {
-      const aDay = dayjs(a.reg_date, 'YYYY-MM-DD HH:mm:ss');
-      const bDay = dayjs(b.reg_date, 'YYYY-MM-DD HH:mm:ss');
-      return bDay.valueOf() - aDay.valueOf();
-    });
+const Page = () => {
+  const { data } = useReviewQuery();
 
   return (
-    <ClientLayout refreshDelay={REVALIDATE * 1000} selectedKey="review">
+    <ClientLayout selectedKey="review">
       <Alert
         style={{ marginBottom: 24 }}
         title="스트리머별 첫 번째 페이지만 불러옵니다."
         type="info"
       />
       <Flex gap="small" wrap>
-        {vodsReviewList.map((item) => (
+        {data.map((item) => (
           <VodCard data={item} key={item.title_no} />
         ))}
       </Flex>
